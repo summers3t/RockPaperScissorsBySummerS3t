64e4a584727f09ea6bc8531b3354de13ca51c71a
summers3t <123099910+summers3t@users.noreply.github.com>
2023-01-30 19:04:02 +0200

    Added game statistics
    Added total wins, loses and draws. After exiting the game the player can see the statistics.

--- rock_paper_scissors_game.py
+++ rock_paper_scissors_game.py
@@ -4,6 +4,9 @@ rock = "Rock"
 paper = "Paper"
 scissors = "Scissors"
 game_counter = 0
+wins = 0
+loses = 0
+draws = 0
 
 player_move = input("Choose [r]ock, [p]aper, [s]cissors or [e]xit: ")
 while player_move != "e":
@@ -16,7 +19,9 @@ while player_move != "e":
     elif player_move == "s":
         player_move = scissors
     else:
-        raise SystemExit("Invalid Input. Try again...")
+        print("Invalid move. Please try again...")
+        player_move = input("Choose [r]ock, [p]aper, [s]cissors or [e]xit: ")
+        continue
 
     computer_random_number = random.randint(1, 3)
 
@@ -34,12 +39,19 @@ while player_move != "e":
     if player_move == rock and computer_move == scissors or \
             player_move == paper and computer_move == rock or \
             player_move == scissors and computer_move == paper:
+        wins += 1
         print("You win!")
     elif player_move == computer_move:
+        draws += 1
         print("Draw!")
     else:
+        loses += 1
         print("You lose!")
     player_move = input("Choose [r]ock, [p]aper, [s]cissors or [e]xit: ")
 else:
     print("Thank you for playing!")
+    print(f"Total wins: {wins}")
+    print(f"Total loses: {loses}")
+    print(f"Total draws: {draws}")
+
     raise SystemExit(f"Total games played: {game_counter}")
